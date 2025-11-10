c17a8811bf5c641214569409caa3383978e94e0c
Zane Mouakket <ZaneMouakket@my.unt.edu>
2025-11-10 16:25:48 -0600

    Add interfact and first command

--- main.java
+++ main.java
@@ -111,6 +111,7 @@ public class main {
                         timeKept INT,
                         customerID VARCHAR(255),
                         model VARCHAR(255),
+                        salePrice INT,
                         FOREIGN KEY (color, model) REFERENCES colors(color, model),
                         FOREIGN KEY (engineType, model) REFERENCES engineTypes(engineType, model),
                         FOREIGN KEY (model) REFERENCES models(model),
@@ -158,6 +159,61 @@ public class main {
         }
     }
 
+    public static void showOptionsAndGetSelectedForever() {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println(
+                    """
+			        Select which command you want to do:
+                    st 3 - Show SALES TRENDS for various brands over the past 3 years (can be any number).
+                    fvp 2.0L Engine - FIND VIN numbers for cars which were made with a given PART 2.0L Engine (can be any part).
+                    tb 2 1 - FIND The top 2 BRANDS by revinue in the past 1 year (can be any number of brands or years).
+                    fmb Convertibles - FIND the MONTH which has the BEST revinue for Convertibles (can be any vehicle type).
+                    ftatv F150 - FIND the dealers which have the TOP AVERAGE TIME a given VEHICLE model is kept (can be any model).
+		            """
+                );
+                System.out.print("Input ('exit' to exit): ");
+                String command = scanner.nextLine();
+                if (command.equalsIgnoreCase("exit")) {
+                    break;
+                } else if(command.startsWith("st")) {
+                    try {
+                        int years = Integer.parseInt(command.substring(3, command.length()));
+                        String result = execute(String.format(
+                            """
+                                SELECT brand, SUM(salePrice) totalSales 
+                                FROM vehicles 
+                                NATURAL JOIN models 
+                                WHERE timeKept <= %d
+                                GROUP BY brand
+                                ORDER BY totalSales;
+                            """
+                        , years * 365));
+                        System.out.println(result);
+                    } catch (SQLException e) {
+                        System.out.println("SQL Error: " + e.getMessage());
+                    }
+                } else if(command.startsWith("fvp")) {
+                    
+                } else if(command.startsWith("tb")) {
+
+                } else if(command.startsWith("fmb")) {
+
+                } else if(command.startsWith("ftatv")) {
+
+                } else if(command.startsWith("sql")) { // for debugging. Ex: 'sql SELECT * FROM vehicles;'
+                    try {
+                        System.out.println(execute(command.substring(4, command.length())));
+                    } catch (SQLException e) {
+                        System.out.println("SQL Error: " + e.getMessage());
+                    }
+                } else {
+                    System.out.println("Invalid Command");
+                }
+
+            }
+    }
+
     // this is just for testing, we wouldn't remove this and put options to select custom queries
     public static void getSQLFromInputForever() {
             Scanner scanner = new Scanner(System.in);
@@ -182,7 +238,7 @@ public class main {
             connect();
             createTablesIfNotExists();
 
-            getSQLFromInputForever();
+            showOptionsAndGetSelectedForever();
         } catch (SQLException e) {
             System.out.println("Connection or setup error: " + e.getMessage());
             e.printStackTrace();
